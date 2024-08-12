2240f2da66c81b7e742ab275af561fc1b600c018
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2024-08-12 14:58:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,57 @@
+function generatePassword(length, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols) {
+  const lowercaseChars = "qwertyuioplkjhgfdsazxcvbnm";
+  const uppercaseChars = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+";
+  let allowedChars = "";
+  let password = "";
 
-
-
-function genaratePassword(length,includeLowerCase, incluseUpperCase,includeNumbers,includeSymbols){
-  const lowercasechars="qwertyuioplkjhgfdsazxcvbnm";
-  const uppercasechars="QWERTYUIOPLKJHGFDSAZXCVBNM";
-  const numberchars="0123456789";
-  const sybmolchars="!@#$%^&*()_+";
-  let allowedchars="";
-  let password="";
-  allowedchars +=  includeLowerCase ? lowercasechars : "";
-  allowedchars += incluseUpperCase ? uppercasechars : "";
-  allowedchars +=includeNumbers ? numberchars : "" ;
-  allowedchars +=includeSymbols ? sybmolchars: "";
-  
-  if(length <=0){
-    return `(password length shound be atleast one)`;
-  }
-  if(allowedchars.length === 0){
-    return `(At least  1 set of character  needs to be selected)`;
-  }
-  for(let i= 0 ; i < length;i++){
-    const randomIndex= Math.floor(Math.random() * allowedchars.length);
-    password +=allowedchars[randomIndex];
+  if (length <= 0) {
+      return `(Password length should be at least one)`;
   }
 
-    return password;
+  allowedChars += includeLowerCase ? lowercaseChars : "";
+  allowedChars += includeUpperCase ? uppercaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
+
+  if (allowedChars.length === 0) {
+      return `(At least one set of characters needs to be selected)`;
+  }
+
+  for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allowedChars.length);
+      password += allowedChars[randomIndex];
+  }
+
+  return password;
 }
 
-const passwordLength=12;
-const includeLowerCase=true;
-const incluseUpperCase=true;
-const includeNumbers=true;
-const includeSymbols=true;
-function submitted(){
+function submitted() {
+  const length = parseInt(document.getElementById("length").value);
+  const includeLowerCase = document.getElementById("lowercase").checked;
+  const includeUpperCase = document.getElementById("uppercase").checked;
+  const includeNumbers = document.getElementById("numbers").checked;
+  const includeSymbols = document.getElementById("symbols").checked;
 
-const password= genaratePassword(passwordLength,
-                            includeLowerCase,incluseUpperCase, 
-                            includeNumbers,includeSymbols)
-                            textbox.textContent=password;
+  const textbox = document.getElementById("textbox");
+  textbox.textContent = "Generating...";
+
+  // Change to the second video
+  const videoSource = document.getElementById("video-source");
+  videoSource.src = "./assets/video2.mp4";
+  const backgroundVideo = document.getElementById("background-video");
+  backgroundVideo.load();
+  backgroundVideo.play();
+
+  // Generate the password after 4 seconds
+  setTimeout(() => {
+      const password = generatePassword(length, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols);
+      textbox.textContent = password;
+
+      // Change back to the first video and stop the second video
+      videoSource.src = "./assets/video1.mp4";
+      backgroundVideo.load();
+      backgroundVideo.play();
+  }, 4000); // Display password and revert video after 4 seconds
 }
-// console.log(`genarated password is : ${password}`)
-
-const textbox = document.getElementById("textbox");
